d3778373df54a6d1095ba51abf2a6c67a4640f45
Anıl Teoman <anilteoman@outlook.com>
2025-04-01 22:01:49 +0300

    sayfalar & validasyonlar 1/2

--- wits7g4Proje/README.md
+++ wits7g4Proje/README.md
@@ -1,12 +1,41 @@
-# React + Vite
+1. Proje oluşturulacak
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Projeyi kendi bilgisayarınızda sıfırdan oluşturarak yapacaksın. (Tercihen vite ile)
+Uygulamanızda bir Login formu (Login.jsx) ve geçici bir success sayfası (Success.jsx) olacak.
+Componentleri "components" klasöründe oluşturmaya dikkat.
 
-Currently, two official plugins are available:
+Login formu: email, şifre ve şartları kabul ediyorum alanından oluşacak.
+Form alanları için validasyonlar eklenecek:
+geçerli email (regex kullanılabilir),
+strong password (regex kullanılabilir)
+ve şartları kabul etmesi gerekecek.
+tüm validasyonları geçer ise login butonu aktif olacak.
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+2. Cypress ile de 2 farklı senaryoda test yazılacak:
 
-## Expanding the ESLint configuration
+a) Başarılı form doldurulduğunda submit edebiliyorum:
 
-If you are developing a production application, we recommend using TypeScript and enable type-aware lint rules. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+success sayfasını açabiliyorum.
+b) Hatalı durumlarda beklenen hata mesajları görünüyor ve buton disabled kalıyor.
+
+email yanlış girdim:
+
+ekranda 1 tane hata mesajı var
+ekranda doğru hata mesajı var
+buton disabled durumda
+email ve password yanlış
+
+ekranda 2 tane hata mesajı var
+ekranda password hata mesajı var
+email ve password doğru ama kuralları kabul etmedim.
+
+buton disabled mı
+
+3. projeyi github’a yükleyeceksin
+
+public olsun
+en az 4 commit olsun
+proje setup’ı bitince
+login sayfası ve success sayfası bitince
+form validasyonları eklenince
+testler eklenince
---
+++ wits7g4Proje/src/components/Login.jsx
@@ -0,0 +1,134 @@
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import {
+  Button,
+  Form,
+  FormFeedback,
+  FormGroup,
+  Input,
+  Label,
+} from "reactstrap";
+
+let initalValues = {
+  email: "",
+  password: "",
+  terms: false,
+};
+
+let errorMessages = {
+  email: "Geçerli bir e-posta adresi girin",
+  password: "Güçlü bir şifre girin",
+  terms: "giriş için şartları kabul et",
+};
+
+const Login = () => {
+  const [formData, setFormData] = useState(initalValues);
+  const [errors, setErrors] = useState({
+    email: false,
+    password: false,
+    terms: false,
+  });
+
+  const [isValid, setIsValid] = useState(false);
+
+  const history=useHistory();
+
+  useEffect(() => {
+    if (
+      validateEmail(formData.email) &&
+      checkPassword(formData.password) &&
+      formData.terms
+    ) {
+      setIsValid(true);
+    } else {
+      setIsValid(false);
+    }
+  }, [formData]);
+
+  const handleChange = (e) => {
+    let { type, checked, name, value } = e.target;
+    value = type == "checkbox" ? checked : value;
+    setFormData({ ...formData, [name]: value });
+
+    if (name == "email") {
+      if (validateEmail(value)) {
+        setErrors({ ...errors, [name]: false });
+      } else {
+        setErrors({ ...errors, [name]: true });
+      }
+    }
+
+    if (name === "password") {
+      if (checkPassword(value)) {
+        setErrors({ ...errors, [name]: false });
+      } else {
+        setErrors({ ...errors, [name]: true });
+      }
+    }
+  };
+
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
+  function checkPassword(password) {
+    var re = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+    return re.test(password);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    
+
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <FormGroup>
+        <Label for="exampleEmail">Email:</Label>
+        <Input
+          id="exampleEmail"
+          name="email"
+          placeholder="Email"
+          type="email"
+          onChange={handleChange}
+          value={formData.email}
+          invalid={errors.email}
+        />
+        {errors.email && <FormFeedback>{errorMessages.email}</FormFeedback>}
+      </FormGroup>
+      <FormGroup>
+        <Label for="examplePassword">Şifre:</Label>
+        <Input
+          id="examplePassword"
+          name="password"
+          placeholder="sifre"
+          type="password"
+          onChange={handleChange}
+          value={formData.password}
+          invalid={errors.password}
+        />
+        {errors.password && (
+          <FormFeedback>{errorMessages.password}</FormFeedback>
+        )}
+      </FormGroup>
+      <FormGroup check>
+        <Input
+          type="checkbox" 
+          name="terms" 
+          value={formData.terms} 
+          onChange={handleChange} 
+        />{" "}
+        <Label check>Şartlari Kabul Ediyorum</Label>
+      </FormGroup>
+      <Button disabled={!isValid}>Submit</Button>
+    </Form>
+  );
+};
+
+export default Login;
